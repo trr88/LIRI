72bfe2253bc88f0a5ae3633258a8c1d8d9e772da
Tom Renicker <trrenicker@gmail.com>
2017-07-06 18:29:38 -0400

    keys removed

--- keys.js
+++ keys.js
@@ -1,16 +1,1 @@
 console.log('this is loaded');
-
-exports.twitterKeys = {
-    consumer_key: 'J7FbHhuRHIBXFUcQzIMofSJk8',
-
-    consumer_secret: 'tyRZ5MfORoC5mKfyQXTemvszr4UO49pAp4h967qzPgBGvOUrZ6',
-
-    access_token_key: '182456511-bUCVcjMoWTfKNlIW9JEqVlXtKUjk3hSZwoe2qSHP',
-
-    access_token_secret: 'TcXVHOwtgzbJ0WhfN8ixLVovY4dmIYNIA9cqGpEUQBY9p'
-}
-
-exports.spotifyKeys = {
-    Client_ID: 'f825a530158c4859bf41f7c321efe9ef',
-    Client_Secret: '18a60acc158c4edda14e8b7a0bd77f6d'
-}
